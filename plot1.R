027848b8d2c7b09de02067d60082fcece37ac6aa
Haneef Awan <haneef.awan@ieee.org>
2015-04-09 13:38:42 +0200

    Rcode

--- plot1.R
+++ plot1.R
@@ -1,1 +1,21 @@
+## Getting the dataset
 
+hpc_raw <- read.csv("./GitHub/Exploratory-Data-Analysis//household_power_consumption.txt", header=T, sep=';', na.strings="?")
+hpc_raw$Date <- as.Date(hpc_raw$Date, format="%d/%m/%Y")
+
+## Create subset 01-02-2007 -- 02-02-2007
+
+hpc_subset <- subset(hpc_raw, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(hpc_raw)
+
+## Date conversion
+datetime <- paste(as.Date(hpc_subset$Date), hpc_subset$Time)
+hpc_subset$Datetime <- as.POSIXct(datetime)
+
+## Plot 1
+hist(hpc_subset$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
