ece822295f4c8db99a14443e76c9f84805c33d04
Haneef Awan <haneef.awan@ieee.org>
2015-04-09 13:52:12 +0200

    Plot 1

--- plot1.R
+++ plot1.R
@@ -16,6 +16,6 @@ hpc_subset$Datetime <- as.POSIXct(datetime)
 hist(hpc_subset$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-## Saving to file
+## Saving to .png file
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
